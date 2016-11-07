1ba257c219171df483114b8f6e806b99c57f4f78
Cherenet <cherenetse@gmail.com>
2016-11-07 14:27:18 -0500

    modifyng the js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,80 +1,214 @@
-
-
- var bio = {
-    "name": "Cherernet Tessema",
-    "role": "Front-end Webdeveloper",
-    "contact": {
+var bio = {
+    "name": "Cherenet Tessema",
+    "role": "Front-End Engineer",
+    "contacts": {
         "email": "cherenetse@gmail.com",
-        "github": "cherenetse.github.io",
+        "github": "cherenetse",
         "linkedin": "https://www.linkedin.com/in/cherenet-alemu-tessema-33ab647a",
-        "location": "Germantown Maryland"
+        "location": "Germantown , Maryland, U.S.A"
     },
-    "welcomeMessage": "welcome to my resume page , I am a self thought developer currently working on Diffrent projects",
-    "skills": ["creative", "commited", "hardworking"
-    ],
-    "biopic": "image/fry.jpg"
+    "picture": "images/bio.jpg",
+    "welcomeMessage": "I'm currently working as a Software Developer in Edmonton, Alberta, Canada.  Thanks for stopping by!",
+    "skills": ["Html", "CSS", "Javascript", "Responsive Webdevelopment"]
 };
 
-
-
- var education = {
-    "schools": [{
-            "name": "Alpha University College",
-            "city": "Addis Ababa",
-            "degree": "Bachelors",
-            "major": ["Marketing Managment", "Information Technology"]
+var work = {
+    "jobs": [
+        {
+            "employer": "FedEx",
+            "title": "Center Consultant",
+            "location": "Germantown , Maryland",
+            "datesWorked": "October 2016",
+            "description": "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag" +
+            "asdfadg asdgasdgaset asdgasdgasdgasdg asdgasdgasgagasg" +
+            "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag" +
+            "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag" +
+            "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag"
         },
+        {
+            "employer": "Safeway",
+            "title": "Cashier",
+            "location": "Germantown, Maryland",
+            "datesWorked": "March 2016 - Jun 2016",
+            "description": "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag" +
+            "asdfadg asdgasdgaset asdgasdgasdgasdg asdgasdgasgagasg" +
+            "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag" +
+            "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag" +
+            "awfaefas asdgfqfaf sadgfageaf asdgasdgagafgaegadfg adfgag"
+        }
+
+    ]
+};
+
+var education = {
+    "schools": [
+        { "name": "Alpha University",
+            "datesAttended": "2009 - 2013",
+            "location": "Addis Ababa, Ethiopia",
+            "degree": "Bachelors Degree",
+            "major": "Marketing Managment",
+            "url": "www.alphauniversitycollege.edu.et"
+        }
+    ],
+    "onlineCourses": [
+        { "school": "Udacity",
+            "title": "Html and CSS",
+            "completed": "September 2016",
+            "url": "https://www.udacity.com/nanodegree/nd015"
+        },
+        { "school": "Linkedin Learning",
+            "title": "Javascript",
+            "completed": "November 2016",
+            "url": "https://www.linkedin.com/learning"
+        }
+
+    ]
+};
+
+var projects = {
+    "projects": [
 
         {
-            "name": "Udacity",
-            "city": "online",
-            "degree": "Nano Degree",
-            "major": "Front-end Webdeveloper"
+            "title": "Online Portfolio",
+            "datesWorked": "September 2016",
+            "description": "Created an online portfolio as part of Udacity's Front-End Web Developer " +
+            "Nanodegree.",
+            "images": ["images/portfolio.png"],
+            "url": "http://cherenetse.github.io/"
+        },
+        {
+            "title": "Candle Shop website",
+            "datesWorked": "March 2016 - May 2016",
+            "description": "Created this Website as a progress for my Front-End development goal",
+            "images": ["images/capture2.png"],
+            "url": "http://sccandles.x10host.com/"
         }
     ]
 };
 
- var work = {
-    "employer": "Fedex Office",
-    "position" : "center Consultant"
-};
 
- var projects = {
-    "projects": [
-    {
-        "title": "candle shop website",
-        "dates": "2016",
-        "description" : " This is my first website to build and every time it will be updated to show my current status",
-        "images": [
-            "https://s3.amazonaws.com/accredible-api-projects/previews/6850/large/1472009673494?1472009443",
-            ]
-    }]
- };
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
 
 
+var formattedContactInfo = [];
+formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
 
- if (bio.skills.length > 0) {
-    $('#header').append(HTMLskillsStart);
-    var formattedSkill = HTMLskills.replace("%data%",
-        bio.skills[0]);
-    $("#skills").append(formattedSkill);
-        formattedSkill = HTMLskills.replace("%data",
-            bio.skills[2]);
-    $("#skills").append(formattedSkill);
-    formattedSkill = HTMLskills.replace("%data",
-            bio.skills[3]);
-    $("#skills").append(formattedSkill);
- };
 
- for (job in work.jobs) {
-    $("#workExperience").append(HTMLworkStart);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedBioPic);
 
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    $(".work-entry:last").append(formattedEmployerTitle);
 
-     };
+if(bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+
+    for(i in bio.skills) {
+        $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+    }
+}
+
+for(i in formattedContactInfo) {
+    $("#topContacts").append(formattedContactInfo[i]);
+    $("#footerContacts").append(formattedContactInfo[i]);
+}
+
+
+function displayWork() {
+
+    if(work.jobs.length > 0) {
+
+        $("#workExperience").append(HTMLworkStart);
+
+        for(i in work.jobs) {
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+            var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+            var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[i].datesWorked);
+            var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+
+            var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
+
+            $(".work-entry:last").append(formattedEmployerWorkTitle);
+            $(".work-entry:last").append(formattedWorkLocation);
+            $(".work-entry:last").append(formattedDatesWorked);
+            $(".work-entry:last").append(formattedWorkDescription);
+        }
+
+    }
+
+}
+
+displayWork();
+
+
+projects.display = function() {
+    if(projects.projects.length > 0) {
+        for(i in projects.projects) {
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].datesWorked);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+            $(".project-entry:last").append(formattedProjectTitle);
+            $(".project-entry:last").append(formattedProjectDates);
+            $(".project-entry:last").append(formattedProjectDescription);
+
+            for(img in projects.projects[i].images) {
+                var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+                $(".project-entry:last").append(formattedProjectImage);
+            }
+
+
+        }
+    }
+}
+
+projects.display();
+
+education.display = function() {
+    if(education.schools.length > 0 || education.onlineCourses.length > 0) {
+        for(i in education.schools) {
+            $("#education").append(HTMLschoolStart);
+
+            var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+            var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+
+
+            $(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+            $(".education-entry:last").append(formattedSchoolDates);
+            $(".education-entry:last").append(formattedSchoolLocation);
+            $(".education-entry:last").append(formattedSchoolMajor);
+
+        }
+
+        if(education.onlineCourses.length > 0) {
+            $("#education").append(HTMLonlineClasses);
+            for(i in education.onlineCourses) {
+                $("#education").append(HTMLschoolStart);
+                var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+                var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+                var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
+                var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+
+                $(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+                $(".education-entry:last").append(formattedOnlineDates);
+                $(".education-entry:last").append(formattedOnlineURL);
+            }
+        }
+
+    }
+}
+
+education.display();
+
 
 
 
